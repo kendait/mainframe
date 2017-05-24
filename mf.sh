d8d5bda13c96b312a68fb201b46f1dc9047878b6
Kenneth Dait <kenanigans@icloud.com>
2017-05-24 07:24:53 -0500

    + Refactoring mf_print

--- mf.sh
+++ mf.sh
@@ -1,43 +1,47 @@
 #!/usr/local/bin/bash
 
 initMainFunctions() {
-	highlightColor="\033[31;40m"
-	colorReset="\033[0m"
 	mf_print() {
+		mainColor="\033[37m"
+		highlightColor="\033[31;40;1m"
+		colorReset="\033[0m"
 		formatSpecs="$1"
 		shift
 		inputString="$@"
-		prependString=""
-		appendString=""
-		case ${formatSpecs:0:1} in
-			0) prependString="";;
-			1) prependString="\n";;
-		esac
-		case ${formatSpecs:1:1} in
-			0) prependString=$prependString;;
-			1) prependString=$prependString"\t";;
-		esac
-		if [[ ${formatSpecs:3:1} -gt 0 ]]; then
-			prependString=$prependString$highlightColor
-			if [[ ${formatSpecs:4:1} -eq 1 ]]; then
-				prependString=$prependString" "
-			fi
-		fi
-		if [[ ${formatSpecs:4:1} -eq 1 ]]; then
-			appendString=" "
-		fi
-		case ${formatSpecs:2:1} in
-			0) appendString=$appendString;;
-			1) appendString=$appendString"\n";;
-			2) appendString=$appendString"\n\n";;
-		esac
-		resultString="${prependString}${inputString}${appendString}"
-		if [ ! -z ${formatSpecs:3:1} ]; then
-			case ${formatSpecs:3:1} in
-				0) resultString=$resultString;;
-				1) resultString=$resultString$colorReset;;
+		buildResultString() {
+			prependString=""
+			appendString=""
+			case ${formatSpecs:0:1} in
+				0) prependString="";;
+				1) prependString="\n";;
 			esac
-		fi
+			case ${formatSpecs:1:1} in
+				0) prependString=$prependString;;
+				1) prependString=$prependString"\t";;
+			esac
+			if [[ ${formatSpecs:3:1} -gt 0 ]]; then
+				prependString=$prependString$highlightColor
+				if [[ ${formatSpecs:4:1} -eq 1 ]]; then
+					prependString=$prependString" "
+				fi
+			else
+				prependString=$prependString$mainColor
+				if [[ ${formatSpecs:4:1} -eq 1 ]]; then
+					prependString=$prependString" "
+				fi
+			fi
+			if [[ ${formatSpecs:4:1} -eq 1 ]]; then
+				appendString=" "
+			fi
+			appendString=$appendString$colorReset
+			case ${formatSpecs:2:1} in
+				0) appendString=$appendString;;
+				1) appendString=$appendString"\n";;
+				2) appendString=$appendString"\n\n";;
+			esac
+		}
+		buildResultString
+		resultString="${prependString}${inputString}${appendString}"
 		echo -en "$resultString"
 	}
 }
